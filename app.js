e01a6062b82c31bad54ef8ba71743b6543f1c2e0
Archana Kumar <akumar@gilt.com>
2015-02-23 22:59:20 -0500

    added copy

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var express = require('express')
-  , engine = require('ejs-locals')
-  , bodyParser = require('body-parser')
-  , expressValidator = require('express-validator')
-  , http = require('http')
-  , path = require('path')
-  , _ = require('lodash')
-  , config = require('./config.json');
+var express          = require('express'),
+    engine           = require('ejs-locals'),
+    bodyParser       = require('body-parser'),
+    expressValidator = require('express-validator'),
+    http             = require('http'),
+    path             = require('path'),
+    _                = require('lodash'),
 
+    global           = require('./copy/global.json'),
+    about            = require('./copy/about.json'),
+    people           = require('./copy/people.json'),
+    reports          = require('./copy/reports.json'),
+    media            = require('./copy/media.json'),
+    newsletters      = require('./copy/newsletters.json'),
+    events           = require('./copy/events.json'),
+    careers          = require('./copy/careers.json'),
 
-var app = express();
+    app = express();
 
 app.engine('ejs', engine);
 
@@ -28,7 +35,18 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res) {
 
-  res.render('index', config);
+  var template = {
+    global      : global,
+    about       : about,
+    people      : people,
+    reports     : reports,
+    media       : media,
+    newsletters : newsletters,
+    events      : events,
+    careers     : careers
+  };
+
+  res.render('pages/index', template);
 });
 
 http.createServer(app).listen(app.get('port'), function(){
